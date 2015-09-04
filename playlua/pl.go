25adf4e262fdae28b36c3a4ec6e8377ef151b151
govlas <0x3E7047@gmail.com>
2015-09-04 15:17:57 +0300

    lua playground

---
+++ playlua/pl.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/gopherjs/jquery"
+	"github.com/yuin/gopher-lua"
+)
+
+var jq = jquery.NewJQuery
+
+func PlayLua(s string) {
+	L := lua.NewState()
+	defer L.Close()
+	if err := L.DoString(s); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	editor := js.Global.Get("ace").Call("edit", "editor")
+	//	editor.Call("setValue", `print("hello")`)
+	session := editor.Call("getSession")
+	session.Call("setMode", "ace/mode/lua")
+	cons := jq("#cons")
+
+	jq("#run").On("click", func() {
+		PlayLua(editor.Call("getValue").String())
+		cons.SetScrollTop(int(cons.Prop("scrollHeight").(float64)))
+	})
+
+	jq("#clean").On("click", func() {
+		cons.SetHtml("")
+	})
+	//println(editor.Call("getValue"))
+
+	//PlayLua(`print("hello")`)
+
+	//js.Global.Get("document").Call("write", "Hello world!")
+}
